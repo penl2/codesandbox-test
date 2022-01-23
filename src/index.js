c3364bdadfd355516020cd9ab341b9376b7c8a10
penl2 <77254385+penl2@users.noreply.github.com>
2022-01-23 11:13:34 +0900

    update

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+console.log("test");
+
+console.log("JavaScriptを実行しています");
+var elems = document.querySelectorAll(".widget_swell_index a");
+if (!elems) {
+  return;
+}
+
+//var current_flag;
+
+console.log(elems);
+console.log("表示完了");
+console.log(elems.length);
